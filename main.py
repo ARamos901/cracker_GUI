59f6142d660d5dab27be2db09f6167e3753b52ec
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2024-12-21 18:16:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from tkinter import *
 from tkinter import filedialog
 import hashlib 
 import os
+import time
 
 window=Tk()
 
@@ -14,27 +13,52 @@ window.config(background="black")
 lock=PhotoImage(file="unlock.png")
 window.iconphoto(True,lock)
 
-label=Label(window,text="Your personal password cracker",
+Top=Label(window,text="Your personal password cracker",
                 fg="white",
                 bg="#756c6b",
                 relief=RAISED,
                 bd=5)
 
-label.pack(pady=20)
+Top.pack(pady=20)
 
 
 Hash=Entry()
 Hash.pack()
 
-
+Password=None
 def openFile():
     filepath=filedialog.askopenfilename()
-    file=open(filepath,'r')
+    time.sleep(3)
+       
     for word in file:
         enc_wrd = word.strip().encode('utf-8')
         digest = hashlib.md5(enc_wrd).hexdigest()
         
+        if digest == Hash:
+            Password=word.strip()
+            break
+        
+        
     file.close()
+
+message=None
+
+def Message():
+    if Password!=None:
+        message=f"The cracked password is {Password}."
+    else:
+        message="The Password could not be cracked."
+
+Message()     
+
+def AddPass():
+    DisplayPass.insert(END,message)
+
+DisplayPass=Label(window,text="Cracked Password will be appear here.")
+
+
+
+
 
 wordlistButton = Button(
     text="Submit Word List",
@@ -47,12 +71,12 @@ wordlistButton = Button(
     relief=RAISED,
     cursor="hand"
 )
-
 wordlistButton.pack(pady=20)
 
 
 
 
+    
     
 
 window.mainloop()
